fb8cd5e2138f7639ccbb3d575f91ab66c528f536
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-08-23 22:27:31 -0400

    Completed Website

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const h2 = document.createElement("h2");
+h2.id = "tag";
 h2.textContent = "Thank you for joining me!";
 
 document.querySelector("body").appendChild(h2);
+
+
+const image = document.getElementsByTagName("img");
+
+
+image.addEventListener('click', function(){
+    alert("That's me!");
+})
